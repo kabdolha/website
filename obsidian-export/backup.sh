bf58a65da9fb39391c194650b2af6b0fa5c414b3
kabdolha <kabdolha@proton.me>
2025-10-05 21:13:52 -0400

    Created homepage and removed sidebars for now

--- obsidian-export/backup.sh
+++
@@ -1,3 +0,0 @@
-echo $(date) "Beginning rsync to /Users/kabdolha/Amazon\ WorkDocs\ Drive/My\ Documents/Backups/Obsidian/" >>/Users/kabdolha/Documents/notes/notes/backup.log 2>&1
-rsync --archive --verbose --delete --exclude=".DS_Store" /Users/kabdolha/Documents/notes/ /Users/kabdolha/WorkDocs\ Drive/Backups/notes/ >>/Users/kabdolha/Documents/notes/notes/backup.log 2>&1
-echo $(date) "Completed rsync to /Users/kabdolha/Amazon\ WorkDocs\ Drive/My\ Documents/Backups/Obsidian/" >>/Users/kabdolha/Documents/notes/notes/backup.log 2>&1
